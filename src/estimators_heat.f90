91938c2e14e9730066bd640fd4c643c41c1c353a
Christophe Vaillant <christophe.vaillant@gmail.com>
2019-11-13 17:21:55 +0100

    Changed from "centroid of estimator" to "estimator of centroid" type RPMD operator approximation.

--- src/estimators_heat.f90
+++ src/estimators_heat.f90
@@ -38,25 +38,43 @@ contains
     implicit none
     double precision, intent(inout):: x(:,:,:), p(:,:,:)
     double precision, intent(out):: tcfval(:)
-    double precision:: s, energy, prob, stdev
+    double precision:: s, energy, prob, stdev, q1, q2
+    double precision:: centroidxi, centroidpi, centroidxj, centroidpj
     integer:: i,j,k, idof
 
     tcfval(1)=0.0d0
-    do i=1,natom
-       energy=0.0d0
-       do j=1,n
-          ! if (i .lt. natom) energy= energy + &
-          !      (x(j,1,i+1) - x(j,1,i))*interforce(x,i,j)*p(j,1,i)/mass(i)
-          ! if (i .gt. 1) energy= energy + &
-          !      (x(j,1,i) - x(j,1,i-1))*interforce(x,i-1,j)*p(j,1,i)/mass(i)
-          if (i .lt. natom) energy= energy + &
-               0.5d0*(x(j,1,i+1) - x(j,1,i))*interforce(x,i,j)*(p(j,1,i) + p(j,1,i+1))/mass(i)
-          if (i .gt. 1) energy= energy + &
-               0.5d0*(x(j,1,i) - x(j,1,i-1))*interforce(x,i-1,j)*(p(j,1,i-1) + p(j,1,i))/mass(i)
-          if (convection) energy= energy+ p(j,1,i)*siteenergy(p,x,i,j)/mass(i)
-       end do
-       tcfval(1)= tcfval(1)+energy/dble(N)
+    do i=1, natom
+       centroidxi= centroid(x(:,1,i))
+       q1=centroidxi - lattice(1,i)
+       centroidpi= centroid(p(:,1,i))
+       if (i .lt. natom) then
+          centroidxj= centroid(x(:,1,i+1))
+          q2= centroidxj - lattice(1,i+1)
+          centroidpj= centroid(p(:,1,i+1))
+          tcfval(1)= tcfval(1) + &
+               0.5d0*(centroidpi + centroidpj)*interharm**2*(centroidxj-centroidxi)*(q1-q2)
+       end if
+       if (i .gt. 1) then
+          centroidxj= centroid(x(:,1,i-1))
+          q2= centroidxj - lattice(1,i-1)
+          centroidpj= centroid(p(:,1,i-1))
+          tcfval(1)= tcfval(1) + &
+               0.5d0*(centroidpi + centroidpj)*interharm**2*(centroidxi-centroidxj)*(q2-q1)
+       end if
     end do
+
+    ! tcfval(1)=0.0d0
+    ! do i=1,natom
+    !    energy=0.0d0
+    !    do j=1,n
+    !       if (i .lt. natom) energy= energy + &
+    !            0.5d0*(x(j,1,i+1) - x(j,1,i))*interforce(x,i,j)*(p(j,1,i) + p(j,1,i+1))/mass(i)
+    !       if (i .gt. 1) energy= energy + &
+    !            0.5d0*(x(j,1,i) - x(j,1,i-1))*interforce(x,i-1,j)*(p(j,1,i-1) + p(j,1,i))/mass(i)
+    !       if (convection) energy= energy+ p(j,1,i)*siteenergy(p,x,i,j)/mass(i)
+    !    end do
+    !    tcfval(1)= tcfval(1)+energy/dble(N)
+    ! end do
 
     tcfval(2)= tcfval(1)
 
